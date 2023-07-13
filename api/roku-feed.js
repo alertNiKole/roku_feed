f0d4833dbd50d8e120f8b9bbee2bada33d8b3b5d
NiKole Maxwell <alertnikole@gmail.com>
2023-07-13 16:08:26 -0400

    switched to env variables

--- api/roku-feed.js
+++ api/roku-feed.js
@@ -1,52 +1,69 @@
-//Ah those requriements and variables
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const fs = require('fs');
-const xml2js = require('xml2js');
+// Import dependencies
+const fetch = require('node-fetch');
+const parseString = require('xml2js').parseString;
 const moment = require('moment');
+const podcastFeedURL = process.env.ANCHOR_RSS_FEED_URL;
 
 module.exports = async (req, res) => {
-  const data = fs.readFileSync('podcast-rss.xml');
-  const result = await xml2js.parseStringPromise(data);
+  try {
+    console.log('Roku Feed Initialized...');
 
-  // Transformed data object
-  const transformedData = {
-    providerName: 'MaxDistro Live',
-    lastUpdated: new Date().toISOString(),
-    language: 'en',
-    series: [
-      {
-        id: 'MDL_Meta_Mind_Shift_Bonus',
-        title: 'Meta Mind Shift Show',
-        seasons: [
-          {
-            seasonNumber: 2,
-            episodes: result.rss.channel[0].item.map((item, index) => ({
-              id: `MDL_MetaMindShift_S002E${index}`,
-              title: item.title[0],
-              content: {
-                streamUrl: item.enclosure[0].$.url,
-                contentType: "mp4",
-                language: "en"
-              },
-              thumbnail: item['itunes:image'] ? item['itunes:image'][0].$.href : '',
-              releaseDate: moment(item.pubDate[0]).toISOString(),
-              shortDescription: item.description[0],
-              longDescription: item['content:encoded'] ? item['content:encoded'][0] : ''
-            }))
-          }
-        ],
-        genres: ['technology', 'sci-fi', 'gaming', 'science-fiction', 'educational']
+    // Fetch the XML data from the Anchor RSS feed
+    const response = await fetch(podcastFeedURL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.text();
+
+    parseString(data, async (err, result) => {
+      if (err) {
+        throw new Error(`Parse error! message: ${err.message}`);
       }
-    ]
+
+      console.log('Parsing is in progress');
+
+      // Transformed data object
+      const transformedData = {
+        providerName: 'MaxDistro Live',
+        lastUpdated: new Date().toISOString(),
+        language: 'en',
+        series: [
+          {
+            id: 'MDL_Meta_Mind_Shift_Bonus',
+            title: 'Meta Mind Shift Show',
+            seasons: [
+              {
+                seasonNumber: 2,
+                episodes: result.rss.channel[0].item.map((item, index) => ({
+                  id: `MDL_MetaMindShift_S002E${index}`,
+                  title: item.title[0],
+                  content: {
+                    streamUrl: item.enclosure[0].$.url,
+                    contentType: "mp4",
+                    language: "en"
+                  },
+                  thumbnail: item['itunes:image'] ? item['itunes:image'][0].$.href : '',
+                  releaseDate: moment(item.pubDate[0]).toISOString(),
+                  shortDescription: item.description[0],
+                  longDescription: item['content:encoded'] ? item['content:encoded'][0] : ''
+                }))
+              }
+            ],
+            genres: ['technology', 'sci-fi', 'gaming', 'science-fiction', 'educational']
+          }
+        ]
+      }
+
+      console.log('Parsing has completed');
+
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(transformedData));
+    });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500);
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(`Internal server error: ${error.message}`);
   }
-
-  res.status(200);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(transformedData));
 };
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-});
